287d3a4ee0a3451c5dc9e0213a33c2a978f583ac
samsuryatal <suryatalsam3@gmail.com>
2025-04-06 18:37:35 +0530

    Add files via upload

---
+++ blocked-uids.js
@@ -0,0 +1,10 @@
+// FILE: blocked-uids.js
+const blockedUIDs = [
+  "123456789",
+  "998877665",
+  "445566778",
+  "hacker123",
+  "toxicplayer001"
+];
+
+// You can manually add/remove UID strings from this list to block/unblock users.
